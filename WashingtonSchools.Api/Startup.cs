3b56379968da4976a748a4845070ab1210e0119d
Shimmy Weitzhandler <2716316+weitzhandler@users.noreply.github.com>
2019-08-11 09:57:18 +0300

    update disable end points on MvcCore

--- WashingtonSchools.Api/Startup.cs
+++ WashingtonSchools.Api/Startup.cs
@@ -34,9 +34,8 @@ namespace WashingtonSchools.Api
       services.AddDbContext<WSDbContext>(options => options.UseSqlServer(connection));
       services.AddOData();
 
-      services.AddMvc(options =>
-      options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
+      services.AddMvcCore(service => service.EnableEndpointRouting = false);
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
